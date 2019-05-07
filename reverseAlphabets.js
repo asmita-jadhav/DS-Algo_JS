6b15cfc783b79df2452eff7cf2ad3413cec8a9c4
JADHAV Asmita 00042805 <asmita.jadhav@3ds.com>
2019-05-07 18:48:19 +0530

    rever char

--- reverseAlphabets.js
+++ reverseAlphabets.js
@@ -7,8 +7,24 @@ Output: #d$c%b&a*
 
 let input='#a$b%c&d';
 
-let output=[];
+let temp=[];
+let tempchar='';
+for(let c=0;c<input.length;c++){
 
+    tempchar=input[c].charCodeAt();
+    if(tempchar>=65 && tempchar<=90 || tempchar>=97 && tempchar<=122){
+        temp.push(input[c]);
+    }
+}
 
+let temp1=temp.reverse();
+
+console.log(temp1);
+
+for(let c=0;c<temp1.length;c++){        
+    input[c]=temp1[c];
+}
+
+console.log(input);
 
 ///Complexity
